41c57d518d12d638ca355e9d5fdfb26156c6f588
Phoebe Oigara <phoebeoigara@gmail.com>
2023-04-02 23:53:35 +0300

    functions that record employee data and manipulate employee details

--- index.js
+++ index.js
@@ -1,4 +1,81 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  return {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+}
+
+function createEmployeeRecords(employeeData) {
+  return employeeData.map(function(emp) {
+    return createEmployeeRecord(emp);
+  });
+}
+
+function createTimeInEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date
+  });
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date
+  });
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find(function(event) {
+    return event.date === date;
+  });
+
+  let timeOut = this.timeOutEvents.find(function(event) {
+    return event.date === date;
+  });
+
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  let hoursWorked = hoursWorkedOnDate.call(this, date);
+  return hoursWorked * this.payPerHour;
+}
+
+// function allWagesFor() {
+//   let dates = this.timeInEvents.map(function(event) {
+//     return event.date;
+//   });
+
+//   let payable = dates.reduce(function(memo, date) {
+//     return memo + wagesEarnedOnDate.call(this, date);
+//   }.bind(this), 0);
+
+//   return payable;
+//}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(function(employee) {
+    return employee.firstName === firstName;
+  });
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce(function(memo, record) {
+    return memo + allWagesFor.call(record);
+  }, 0);
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
